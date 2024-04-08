1c1bb47ff3c2bdc19d9ad7b601d9371bb75d9faa
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-04-08 17:17:09 -0400

    added user service to client/services

---
+++ client/src/services/userService.js
@@ -0,0 +1,7 @@
+import { REACT_APP_API_URL, api } from "./config";
+
+const USER_API_URL = `${REACT_APP_API_URL}/user`;
+
+// This file will be the user service for a User object. 
+// Once implemented, this file will contain the functionality of the API endpoints 
+// and how they should be called, and with which parameters if necessary
