8881a8dd5db69d6eeeb51751dadf7905b7d3366a
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-04-08 17:21:30 -0400

    added user controller

---
+++ server/controller/user.js
@@ -0,0 +1,4 @@
+// This file will be the user controller
+// Once implemented, this file will define all necessary functions for a user object in the model.
+// Functionality of the user controller will cover user registration for the platform, creating and editing
+// a profile, and more.
