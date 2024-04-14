4abc60dda43c4a211ea10c166e63116e8cf11028
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-04-14 19:27:29 -0400

    added editQuestion, tested and working in Postman

--- server/controller/question.js
+++ server/controller/question.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Question = require("../models/questions");
 //const Answer = require("../models/answers");
 const { addTag, getQuestionsByOrder, filterQuestionsBySearch } = require('../utils/question');
+const { authRequired } = require("../utils/authMiddleware"); // import middleware for authenticating user
 
 const router = express.Router();
 
@@ -80,9 +81,44 @@ const addQuestion = async (req, res) => {
     }
 };
 
+// Edit Question (first without authenticating user)
+const editQuestion = async (req, res) => {
+
+    const { qid } = req.params;
+    const userId = req.session.userId; // user ID from session (must match Question.askedBy reference)
+    const updateData = req.body;
+
+    try {
+        // First, find question and ensure it exists and is askedBy current userId stored in session
+        const question = await Question.findById(qid);
+
+        if (!question) {
+            return res.status(404).json({ error: 'Question not found' });
+        }
+
+        // check if logged in user is the one who asked the question
+        if (question.asked_by.toString() !== userId) {
+            return res.status(403).json({ error: 'Unauthorized: You are not the author of this question'});
+        }
+
+        // Update the question if the user is authorized
+        const updatedQuestion = await Question.findByIdAndUpdate(qid, updateData, { new: true });
+        if (!updatedQuestion) {
+            return res.status(404).json({ error: 'Unable to update the question' });
+        }
+
+        res.status(200).json(updatedQuestion);
+
+    } catch (error) {
+        console.error('Error updating question: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
 // add appropriate HTTP verbs and their endpoints to the router
 router.get("/getQuestion", getQuestionsByFilter);
 router.get("/getQuestionById/:qid", getQuestionById);
-router.post("/addQuestion", addQuestion);
+router.post("/addQuestion", addQuestion); // need to add authRequired middleware here too
+router.put("/editQuestion/:qid", authRequired, editQuestion);
 
 module.exports = router;
