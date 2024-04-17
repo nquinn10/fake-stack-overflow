258a1b23faac468af77b52c5a66559647578502f
nquinn10 <ntquinn10@gmail.com>
2024-04-17 19:08:35 -0400

    /question/addQuestion unauthenticated user test

--- server/tests/unauthenticatedUser.test.js
+++ server/tests/unauthenticatedUser.test.js
@@ -154,6 +154,16 @@ describe('Unauthorized Access to /user/ endpoints ', () => {
         expect(response.text).toContain("Unauthorized access");
     });
 
+    // ***************************** test post /question/addQuestion endpoint ***********************************
+    it("should return 401 unauthorized if no userId in session", async () => {
+        const response = await supertest(server)
+            .post("/question/addQuestion")
+            .send({ qid: "dummyQuestionId", ans: { text: "Sample answer" } });
+
+        expect(response.status).toBe(401);
+        expect(response.text).toContain("Unauthorized access");
+    });
+
 
 
 
