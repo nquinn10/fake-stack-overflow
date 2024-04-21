3f740c8cf6a215098d8146432bf974e10a30885b
nquinn10 <ntquinn10@gmail.com>
2024-04-21 16:19:33 -0400

    added editQuestion endpoint

--- client/src/services/questionService.js
+++ client/src/services/questionService.js
@@ -25,4 +25,11 @@ const addQuestion = async (q) => {
     return res.data;
 };
 
-export { getQuestionsByFilter, getQuestionById, addQuestion };
+// To edit Questions
+const editQuestion = async (qid, q) => {
+    const res = await api.put(`${QUESTION_API_URL}/editQuestion/${qid}`, q);
+
+    return res.data;
+};
+
+export { getQuestionsByFilter, getQuestionById, addQuestion, editQuestion};
