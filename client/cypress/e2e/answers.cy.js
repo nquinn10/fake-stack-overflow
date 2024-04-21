14be8c3a3c9d6d1472aae91e79ba9911d3c5a1e8
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-04-21 18:06:10 -0400

    updated answers e2e test to init/destroy DB before/after each

--- client/cypress/e2e/answers.cy.js
+++ client/cypress/e2e/answers.cy.js
@@ -1,4 +1,15 @@
 describe('Answer Page 1', () => {
+
+    beforeEach(() => {
+        // Seed the database before each test
+        cy.exec("node ../server/init.js");
+      });
+    
+      afterEach(() => {
+        // Clear the database after each test
+        cy.exec("node ../server/destroy.js");
+      });
+    
     it('Answer Page displays expected header', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Programmatically navigate using React router').click();
@@ -8,22 +19,19 @@ describe('Answer Page 1', () => {
         cy.get('#sideBarNav').should('contain', 'Questions');
         cy.get('#sideBarNav').should('contain', 'Tags');
     })
-})
 
-describe('Answer Page 2', () => {
+
     it('Answer Page displays expected question text', () => {
         const text = "the alert shows the proper index for the li clicked, and when I alert the variable within the last function Im calling, moveToNextImage(stepClicked), the same value shows but the animation isnt happening. This works many other ways, but Im trying to pass the index value of the list item clicked to use for the math to calculate.";
         cy.visit('http://localhost:3000');
         cy.contains('Programmatically navigate using React router').click();
-        cy.get('#questionBody').should('contain', ''); // added vote count
         cy.get('.answer_question_text').should('contain', text);
         cy.get('#questionBody').should('contain', 'betty_j');
         cy.get('#questionBody').should('contain', 'Jan 20, 2022');
         cy.get('#questionBody').should('contain', '03:00');
     })
-})
 
-describe('Answer Page 3', () => {
+
     it('Answer Page displays expected answers', () => {
         const answers = ["React Router is mostly a wrapper around the history library. history handles interaction with the browser's window.history for you with its browser and hash histories. It also provides a memory history which is useful for environments that don't have a global history. This is particularly useful in mobile app development (react-native) and unit testing with Node.", "On my end, I like to have a single history object that I can carry even outside components. I like to have a single history.js file that I import on demand, and just manipulate it. You just have to change BrowserRouter to Router, and specify the history prop. This doesn't change anything for you, except that you have your own history object that you can manipulate as you want. You need to install history, the library used by react-router."];
         cy.visit('http://localhost:3000');
@@ -32,9 +40,8 @@ describe('Answer Page 3', () => {
             cy.wrap($el).should('contain', answers[index]);
         });
     });
-});
 
-describe('Answer Page 4', () => {
+
     it('Answer Page displays expected authors', () => {
         const authors = ['betty_j', 'sammysmith'];
         const date = ['Nov 20','Nov 23'];
@@ -48,15 +55,14 @@ describe('Answer Page 4', () => {
         });
     });
 
-describe('Answer Page 5', () => {
+
     it('Answer Page displays expected vote counts', () => {
         const votes = ['1', '-1'];
         cy.visit('http://localhost:3000');
         cy.contains('Programmatically navigate using React router').click();
-        cy.get('.answer_question_vote').each(($el, index) => {
+        cy.get('.answerVote').each(($el, index) => {
             cy.wrap($el).should('contain', votes[index]);;
         });
     });
-});
 
 });
