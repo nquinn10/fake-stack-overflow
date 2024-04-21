9402e64f58d76e62260c2d90b3f2324b5253b7b5
nquinn10 <ntquinn10@gmail.com>
2024-04-21 19:32:19 -0400

    edit user answer impl

--- client/src/components/main/userProfilePage/profileBody/editAnswerForm.js
+++ client/src/components/main/userProfilePage/profileBody/editAnswerForm.js
@@ -7,12 +7,11 @@ const EditAnswerForm = ({ answer, onSave, onCancel }) => {
     const [text, setText] = useState(answer.text);
 
     const handleSubmit = async (e) => {
-        e.preventDefault();;
+        e.preventDefault();
         try {
             await editAnswer(answer._id, { text });
-            onSave(); // Use the onSave prop to handle the updated question in the parent component
+            onSave();
         } catch (error) {
-            // Handle errors such as displaying error messages
             console.error('Failed to save the question:', error);
         }
     };
