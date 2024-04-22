34f066f335e0d30ff8b0d9e9060d8fa8cb9936c0
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-04-21 22:28:36 -0400

    updated search e2e test to init/destroy DB, update postMod e2e test and separated reset/delete into 2 distinct tests

--- client/cypress/e2e/postModeration.cy.js
+++ client/cypress/e2e/postModeration.cy.js
@@ -1,4 +1,15 @@
-describe('Post Moderation 1', () => {
+describe('Post Moderation Tests', () => {
+
+    beforeEach(() => {
+        // Seed the database before each test
+        cy.exec("node ../server/init.js");
+    });
+    
+    afterEach(() => {
+        // Clear the database after each test
+        cy.exec("node ../server/destroy.js");
+    });
+
     it('Show error page for non-admin user', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Login').click();
@@ -9,9 +20,8 @@ describe('Post Moderation 1', () => {
         cy.get("#postModerationButton").click();
         cy.contains('Oops!'); // Error message on screen if non-admin user logged in
     })
-})
 
-describe('Post Moderation 2', () => {
+
     it('Show flagged content for admin user', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Login').click();
@@ -23,9 +33,8 @@ describe('Post Moderation 2', () => {
         cy.contains('Flagged Questions'); // If admin user logged in, flagged content will be shown to review
         cy.contains('Flagged Answers');
     })
-})
 
-describe('Post Moderation 3', () => {
+
     it('Shows the reset and delete buttons', () => {
         cy.visit('http://localhost:3000');
         cy.contains('Login').click();
@@ -34,10 +43,45 @@ describe('Post Moderation 3', () => {
         cy.get('#loginButton').click();
         cy.contains('All Questions'); // should take you to home page
         cy.get("#postModerationButton").click();
+        cy.contains("Reset");
+        cy.contains("Delete");
+    })
+
+    it('Resets answer vote count to 0 if reset button clicked', () => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Login').click();
+        cy.get('#loginEmailInput').type('sammy@email.com');
+        cy.get('#loginPasswordInput').type('GHJK543');
+        cy.get('#loginButton').click();
+        cy.contains('All Questions'); // should take you to home page
+        cy.get("#postModerationButton").click();
+        cy.contains("Best pizza in Boston?");
+        cy.contains("-16 votes");
         cy.get("#resetBtnQ").click(); // if you click the reset button, question removed from flagged content (reverses flag, sets vote count to 0)
         cy.contains("No flagged questions to review");
-        cy.get("#deleteBtnA").click(); // if you click the delete button, question removed from website
-        cy.contains("No flagged answers to review.");
+        cy.get("#menu_question").click();
+        cy.contains("All Questions");
+        cy.contains("Best pizza in Boston?").click();
+        cy.contains('0'); // vote count reset to 0 to reverse flag
+    })
+
+    it("Deletes answer from database and decrements answer count on question", () => {
+        cy.visit('http://localhost:3000');
+        cy.contains('Login').click();
+        cy.get('#loginEmailInput').type('sammy@email.com');
+        cy.get('#loginPasswordInput').type('GHJK543');
+        cy.get('#loginButton').click();
+        cy.contains('All Questions'); // should take you to home page
+        cy.contains("Quick question about storage on android").click();
+        cy.contains("2 answers");  // before question deleted
+        cy.get("#postModerationButton").click();
+        cy.contains("Blah blah blah blah blah");
+        cy.contains("-15 votes");
+        cy.get("#deleteBtnA").click(); // if you click the delete button, answer removed from DB
+        cy.contains("No flagged answers to review");
+        cy.get("#menu_question").click();
+        cy.contains("Quick question about storage on android").click();
+        cy.contains("1 answers"); // question deleted
     })
 })
 
--- client/cypress/e2e/search.cy.js
+++ client/cypress/e2e/search.cy.js
@@ -1,4 +1,16 @@
-describe('Search 1', () => {
+describe('Search Tests', () => {
+
+    beforeEach(() => {
+        // Seed the database before each test
+        cy.exec("node ../server/init.js");
+    });
+    
+    afterEach(() => {
+        // Clear the database after each test
+        cy.exec("node ../server/destroy.js");
+    });
+    
+
     it('Search string in question text', () => {
         const qTitles = ['android studio save string shared preference, start activity and load the saved string'];
         cy.visit('http://localhost:3000');
@@ -7,9 +19,8 @@ describe('Search 1', () => {
             cy.wrap($el).should('contain', qTitles[index]);
         })
     })
-})
 
-describe('Search 2', () => {
+
     it('Search string matches tag and text', () => {
         const qTitles = ['android studio save string shared preference, start activity and load the saved string', "Programmatically navigate using React router"];
         cy.visit('http://localhost:3000');
@@ -18,9 +29,9 @@ describe('Search 2', () => {
             cy.wrap($el).should('contain', qTitles[index]);
         })
     })
-})
 
-describe('Search 3', () => {
+
+
     it('Output of the search should be in newest order by default', () => {
         const qTitles = ["Quick question about storage on android", "android studio save string shared preference, start activity and load the saved string", "Programmatically navigate using React router"];
         cy.visit('http://localhost:3000');
@@ -29,35 +40,35 @@ describe('Search 3', () => {
             cy.wrap($el).should('contain', qTitles[index]);
         });
     });
-});
 
-describe('Search 4', () => {
+
+
     it('Output of the search should show number of results found', () => {
         const qTitles = ["Quick question about storage on android", "android studio save string shared preference, start activity and load to saved string", "Programmatically navigate using React router"];
         cy.visit('http://localhost:3000');
         cy.get('#searchBar').type('android [react]{enter}');
         cy.contains(qTitles.length+" questions");
     });
-});
 
-describe('Search 5', () => {
+
+
     it('Output of the empty search should show all results ', () => {
         const qTitles = ["Best pizza in Boston?", "Quick question about storage on android", "Object storage for a web application", "android studio save string shared preference, start activity and load the saved string", "Programmatically navigate using React router"];
         cy.visit('http://localhost:3000');
         cy.get('#searchBar').type('{enter}');
         cy.contains(qTitles.length+" questions");
     });
-});
 
-describe('Search 6', () => {
+
+
     it('Search string with non-existing tag and non-tag word', () => {
         cy.visit('http://localhost:3000');
         cy.get('#searchBar').type('[NonExistingTag] nonexistingword{enter}');
         cy.contains('No Questions Found');
     });
-});
 
-describe('Search 7', () => {
+
+
     it('Search string with case-insensitive matching', () => {
         const qTitles = ['android studio save string shared preference, start activity and load the saved string'];
         cy.visit('http://localhost:3000');
