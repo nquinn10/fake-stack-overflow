32a296bf71dc8dc1fc50aa5e788b1120d586f349
nquinn10 <ntquinn10@gmail.com>
2024-04-22 00:15:17 -0400

    edit question error handling

--- client/src/components/main/userProfilePage/profileBody/editQuestionForm/index.js
+++ client/src/components/main/userProfilePage/profileBody/editQuestionForm/index.js
@@ -9,30 +9,55 @@ const EditQuestionForm = ({ question, onSave, onCancel }) => {
     const [text, setText] = useState(question.text);
     const [tags, setTags] = useState(question.tags.map(tag => tag.name).join(' '));
 
+    const [titleError, setTitleError] = useState('');
+    const [textError, setTextError] = useState('');
+    const [tagsError, setTagsError] = useState('');
+
+    const validateInputs = () => {
+        let isValid = true;
+        setTitleError('');
+        setTextError('');
+        setTagsError('');
+
+        if (!title) {
+            setTitleError('Title cannot be empty');
+            isValid = false;
+        }
+        if (!text) {
+            setTextError('Text cannot be empty');
+            isValid = false;
+        }
+        if (!tags) {
+            setTagsError('Must have at least 1 tag');
+            isValid = false;
+        }
+        return isValid;
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (validateInputs()) {
+            try {
+                const tagsArray = tags.split(" ").filter(tag => tag.trim() !== "");
+                const updatedQuestionData = {
+                    title,
+                    text,
+                    tags: tagsArray
+                };
 
-        const tagsArray = tags.split(" ").filter(tag => tag.trim() !== "");
-        const updatedQuestionData = {
-            title,
-            text,
-            tags: tagsArray
-        };
-
-        // Remove any empty fields to avoid overwriting with undefined
-        Object.keys(updatedQuestionData).forEach(key => {
-            if (updatedQuestionData[key] === undefined) {
-                delete updatedQuestionData[key];
+                // Remove any empty fields to avoid overwriting with undefined
+                Object.keys(updatedQuestionData).forEach(key => {
+                    if (updatedQuestionData[key] === undefined) {
+                        delete updatedQuestionData[key];
+                    }
+                });
+                await editQuestion(question._id, updatedQuestionData);
+                onSave(); // Use the onSave prop to handle the updated question in the parent component
+            } catch (error) {
+                console.error('Failed to save the question:', error);
             }
-        });
-
-        try {
-            await editQuestion(question._id, updatedQuestionData);
-            onSave(); // Use the onSave prop to handle the updated question in the parent component
-        } catch (error) {
-            // Handle errors such as displaying error messages
-            console.error('Failed to save the question:', error);
         }
+
     };
 
 
@@ -44,7 +69,7 @@ const EditQuestionForm = ({ question, onSave, onCancel }) => {
                 id={"formTitleInput"}
                 val={title}
                 setState={setTitle}
-                //err={titleErr}
+                err={titleError}
             />
             <Textarea
                 title={"Question Text"}
@@ -52,7 +77,7 @@ const EditQuestionForm = ({ question, onSave, onCancel }) => {
                 id={"formTextInput"}
                 val={text}
                 setState={setText}
-                //err={textErr}
+                err={textError}
             />
             <Input
                 title={"Tags"}
@@ -60,7 +85,7 @@ const EditQuestionForm = ({ question, onSave, onCancel }) => {
                 id={"formTagInput"}
                 val={tags}
                 setState={setTags}
-                //err={tagErr}
+                err={tagsError}
             />
             <div className="btn_indicator_container">
                 <button
