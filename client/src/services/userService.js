6d33a13935eb164a7a72d53b7fccd93b2e79191e
nquinn10 <ntquinn10@gmail.com>
2024-04-23 15:10:35 -0400

    code clean up

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -2,10 +2,6 @@ import { REACT_APP_API_URL, api } from "./config";
 
 const USER_API_URL = `${REACT_APP_API_URL}/user`;
 
-// This file will be the user service for a User object. 
-// Once implemented, this file will contain the functionality of the API endpoints 
-// and how they should be called, and with which parameters if necessary
-
 const login = async (email, password) => {
     const response = await api.post(`${USER_API_URL}/login`, { email, password });
     return response.data;
